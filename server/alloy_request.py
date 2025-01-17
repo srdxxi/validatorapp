2a50c464036b9a22df015a35f712393bfd2fbf44
srdxxi <scott.dixon@outlook.com>
2025-01-17 07:33:47 -0500

    update

--- server/alloy_request.py
+++ server/alloy_request.py
@@ -5,7 +5,7 @@ EVALUATIONS_API = "https://sandbox.alloy.co/v1/evaluations"
 ALLOY_KEY = secret_key = os.environ.get('ALLOY_KEY')
 
 def make_request(data):
-    payload = { "name_first": data.get('name_first'), "name_last": data.get('name_last') }
+    payload = { "name_first": data.get('name_first'), "name_last": data.get('name_last'),  "email": data.get('email'), "birth_date": data.get('birth_date'), "document_ssn": data.get('document_ssn'), "document_ssn": data.get('document_ssn')}
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
